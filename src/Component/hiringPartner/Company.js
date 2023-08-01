0887ab67f7d604d5c2495a88d64f4f488a1383ff
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-08-01 11:57:14 +0530

    company hiring

---
+++ src/Component/hiringPartner/Company.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import style from "./Company.module.css"
+const Company = () => {
+  return (
+    <div className={style.main}>
+      
+    
+      <h4>LEADING COMPANIES BUILD TEAMS ON TOPHIRE</h4>
+        <div className={style.first}>
+            <img src='https://tse1.mm.bing.net/th?id=OIP.B2yTw6ZOt_B8RBSKuW0zMwHaDt&pid=Api&rs=1&c=1&qlt=95&w=181&h=90' />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.XChacERYRI5Ot2MCnOr1cgHaEK&pid=Api&rs=1&c=1&qlt=95&w=155&h=87' />
+            <img src='https://tse1.explicit.bing.net/th?id=OIP.ne-u5ll6BH5v8ntGzdbxYgHaCn&pid=Api&P=0&h=180'style={{height:"3rem"}}/>
+            <img src='https://s.yimg.com/fz/api/res/1.2/ugM3SyjmuzQw5NdNalq2dg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/48b9be84-be3c-38c7-86f4-89de82bd2dc8/t_500x300' style={{height:"2rem"}} />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.sG2nhL92lF6mGi4XaeW9hAHaBs&pid=Api&rs=1&c=1&qlt=95&w=500&h=113'style={{height:"30px"}}/>
+        </div>
+        <div className={style.first}>
+            <img src='https://tse1.mm.bing.net/th?id=OIP.B2yTw6ZOt_B8RBSKuW0zMwHaDt&pid=Api&rs=1&c=1&qlt=95&w=181&h=90' />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.XChacERYRI5Ot2MCnOr1cgHaEK&pid=Api&rs=1&c=1&qlt=95&w=155&h=87' />
+            <img src='https://tse1.explicit.bing.net/th?id=OIP.ne-u5ll6BH5v8ntGzdbxYgHaCn&pid=Api&P=0&h=180'style={{height:"3rem"}}/>
+            <img src='https://s.yimg.com/fz/api/res/1.2/ugM3SyjmuzQw5NdNalq2dg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/48b9be84-be3c-38c7-86f4-89de82bd2dc8/t_500x300' style={{height:"2rem"}} />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.sG2nhL92lF6mGi4XaeW9hAHaBs&pid=Api&rs=1&c=1&qlt=95&w=500&h=113'style={{height:"30px"}}/>
+        </div>
+        <div className={style.first}>
+            <img src='https://tse1.mm.bing.net/th?id=OIP.B2yTw6ZOt_B8RBSKuW0zMwHaDt&pid=Api&rs=1&c=1&qlt=95&w=181&h=90' />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.XChacERYRI5Ot2MCnOr1cgHaEK&pid=Api&rs=1&c=1&qlt=95&w=155&h=87' />
+            <img src='https://tse1.explicit.bing.net/th?id=OIP.ne-u5ll6BH5v8ntGzdbxYgHaCn&pid=Api&P=0&h=180'style={{height:"3rem"}}/>
+            <img src='https://s.yimg.com/fz/api/res/1.2/ugM3SyjmuzQw5NdNalq2dg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/48b9be84-be3c-38c7-86f4-89de82bd2dc8/t_500x300' style={{height:"2rem"}} />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.sG2nhL92lF6mGi4XaeW9hAHaBs&pid=Api&rs=1&c=1&qlt=95&w=500&h=113'style={{height:"30px"}}/>
+        </div>
+        <div className={style.first}>
+            <img src='https://tse1.mm.bing.net/th?id=OIP.B2yTw6ZOt_B8RBSKuW0zMwHaDt&pid=Api&rs=1&c=1&qlt=95&w=181&h=90' />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.XChacERYRI5Ot2MCnOr1cgHaEK&pid=Api&rs=1&c=1&qlt=95&w=155&h=87' />
+            <img src='https://tse1.explicit.bing.net/th?id=OIP.ne-u5ll6BH5v8ntGzdbxYgHaCn&pid=Api&P=0&h=180'style={{height:"3rem"}}/>
+            <img src='https://s.yimg.com/fz/api/res/1.2/ugM3SyjmuzQw5NdNalq2dg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/48b9be84-be3c-38c7-86f4-89de82bd2dc8/t_500x300' style={{height:"2rem"}} />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.sG2nhL92lF6mGi4XaeW9hAHaBs&pid=Api&rs=1&c=1&qlt=95&w=500&h=113'style={{height:"30px"}}/>
+        </div>
+        <div className={style.first}>
+            <img src='https://tse1.mm.bing.net/th?id=OIP.B2yTw6ZOt_B8RBSKuW0zMwHaDt&pid=Api&rs=1&c=1&qlt=95&w=181&h=90' />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.XChacERYRI5Ot2MCnOr1cgHaEK&pid=Api&rs=1&c=1&qlt=95&w=155&h=87' />
+            <img src='https://tse1.explicit.bing.net/th?id=OIP.ne-u5ll6BH5v8ntGzdbxYgHaCn&pid=Api&P=0&h=180'style={{height:"3rem"}}/>
+            <img src='https://s.yimg.com/fz/api/res/1.2/ugM3SyjmuzQw5NdNalq2dg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/48b9be84-be3c-38c7-86f4-89de82bd2dc8/t_500x300' style={{height:"2rem"}} />
+            <img src='https://tse1.mm.bing.net/th?id=OIP.sG2nhL92lF6mGi4XaeW9hAHaBs&pid=Api&rs=1&c=1&qlt=95&w=500&h=113'style={{height:"30px"}}/>
+        </div>
+        
+ 
+    </div>
+  )
+}
+
+export default Company
--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -3,13 +3,15 @@ import React from 'react'
 import Navbar1 from '../../Component/Navbar/Navbar'
 import Description from '../../Component/Description/Description'
 import Step from '../../Component/Step/Step'
-
+import style from "./HomePage.module.css"
+import Company from '../../Component/hiringPartner/Company'
 const HomePage = () => {
   return (
-    <div>
+    <div className={style.main}>
      <Navbar1/>
      <Description/>
      <Step/>
+     <Company/>
     </div>
   )
 }
