a2970413cdbc380b2f3e7975ddfd59caf6c778bd
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-08-02 12:30:32 +0530

    youtube video

--- src/Component/Techies/Techies.js
+++ src/Component/Techies/Techies.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import YouTubeVideo from './Youtube'
 import style from "./Techies.module.css"
+
 const Techies = () => {
     const videoId = 'w0eeQ0Tgk7E';
+    const videoId1= "t8OC41i0ggY";
   return (
     <div className={style.main}>
       <h1>Techies 💘 TopHire</h1>
       <h4>A better love story than twilight. Check the video below to find out why.</h4>
-     <div className={style.youtube}> <YouTubeVideo videoId={videoId} /></div>
+     <div className={style.youtube}> <YouTubeVideo videoId={videoId} />
+    
+     </div>
     </div>
   )
 }
